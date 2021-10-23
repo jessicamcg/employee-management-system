ed99fec12176d81c12a9607e7486cf1f0177ecca
jessicamcg <jessicamcguico@gmail.com>
2021-10-23 11:14:22 -0500

    db dept and role table setup

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,18 @@ CREATE DATABASE human_resources_db;
 USE human_resources_db;
 
 CREATE TABLE department (
-
+    id INT NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role_info (
-
+    id INT NOT NULL,
+    title VARCHAR(25),
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
