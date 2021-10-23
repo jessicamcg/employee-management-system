00346e5c77306ba2b4b26580fcdbcc680bfeb444
jessicamcg <jessicamcguico@gmail.com>
2021-10-23 11:46:00 -0500

    setup employee table

--- db/schema.sql
+++ db/schema.sql
@@ -14,10 +14,17 @@ CREATE TABLE role_info (
     title VARCHAR(25),
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
+    PRIMARY KEY (id)
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
